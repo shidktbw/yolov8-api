9990fec0f371a7bcfd7a5586d13dea7340daabc7
shidktbw <112849918+shidktbw@users.noreply.github.com>
2024-02-13 06:48:36 +0000

    Update app.py

--- app.py
+++ app.py
@@ -15,32 +15,21 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Get the model file from the request
     model_file = request.files['model']
-    # Get the uploaded file from the request
     image_file = request.files['file']
     
-    # Create a unique file name for the model
     model_filename = "models/" + str(random.randint(1, 1000000)) + ".pt"
-    # Save the model file
     model_file.save(model_filename)
 
-    # Create an instance of the YOLO model with the loaded model
     model = YOLO(model_filename)
     
-    # Create a random image file name
     image_filename = str(random.randint(1, 1000000)) + ".jpg"
-    # Save the file to the temporary directory with the new name
     image_file.save(os.path.join('temp', image_filename))
     
-    # Call neural network to process the image
     results = model.predict(source=os.path.join('temp', image_filename),save=True)
     
-    # Get the latest created directory
     latest_dir = max(glob.glob('runs/detect/predict*'), key=os.path.getctime)
-    # Add a 5-second delay
     time.sleep(5)
-    # Return the results to the HTML page
     return render_template('index.html', filename=image_filename, latest_dir=latest_dir)
 
 
